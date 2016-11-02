8382a2b7e6b7973cb6a9dc3ac5d396724a444279
caiocmsouza <caiocesar.msouza@gmail.com>
2016-11-02 12:52:26 +0000

    aula2

---
+++ projeto/aula2-video1/eggplant-brownie/eggplant-brownie/models/Meal.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+class Meal {
+    let name:String
+    let happiness:Int
+    let items:Array<Item>
+    
+    init(name:String, happiness:Int, items: Array<Item> = []) {
+        self.name = name
+        self.happiness = happiness
+        self.items = items
+    }
+    
+    func allCalories() -> Double {
+        var total = 0.0
+        for item in items {
+            total += item.calories
+        }
+        return total
+    }
+    
+}
